773b88c33990ba0926b390e70073da1bad739bea
wes <wes.bovee@gmailcom>
2024-05-08 15:36:26 -0600

    add database schema

--- aisql code/setup.sql
+++ aisql code/setup.sql
@@ -1,48 +1,92 @@
-create table person (
-    person_id integer primary key,
-    name varchar(20) not null
+-- create table person (
+--     person_id integer primary key,
+--     name varchar(20) not null
+-- );
+
+-- create table phone (
+--     phone_id integer primary key,
+--     person_id integer not null,
+--     area_code int not null,
+--     number int not null,
+--     can_recieve_sms tinyint not null,
+--     foreign key (person_id) references person (person_id)
+-- );
+
+-- create table address (
+--     address_id integer primary key,
+--     person_id integer not null,
+--     street varchar(50),
+--     zip integer not null
+-- );
+
+-- create table zip (
+--     zip integer primary key,
+--     city varchar(35),
+--     state_two_letter_code char(2)
+-- );
+
+-- create table dog (
+--     dog_id integer primary key,
+--     name varchar(35),
+--     breed varchar(35),
+--     birth_date date
+-- );
+
+-- create table award (
+--     award_id integer primary key,
+--     dog_id integer not null,
+--     event_date date,
+--     award_name varchar(25) not null,
+--     foreign key (dog_id) references dog (dog_id)
+-- );
+
+-- create table person_dog (
+--     dog_id integer,
+--     person_id integer,
+--     foreign key (dog_id) references dog (dog_id),
+--     foreign key (person_id) references person (person_id)
+-- );
+
+DROP SCHEMA IF EXISTS vending;
+CREATE SCHEMA vending;
+USE vending;
+
+CREATE TABLE Locations (
+  id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  location varchar(255) NOT NULL,
+  total_earnings decimal(9,2),
+  recent_earnings decimal(9,2)
 );
 
-create table phone (
-    phone_id integer primary key,
-    person_id integer not null,
-    area_code int not null,
-    number int not null,
-    can_recieve_sms tinyint not null,
-    foreign key (person_id) references person (person_id)
+CREATE TABLE Machines (
+  id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  location_id integer NOT NULL,
+  total_earnings decimal(9,2),
+  recent_earnings decimal(9,2),
+  FOREIGN KEY (location_id) REFERENCES Locations (id)
 );
 
-create table address (
-    address_id integer primary key,
-    person_id integer not null,
-    street varchar(50),
-    zip integer not null
+CREATE TABLE Products (
+  id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  cost decimal(9,2) NOT NULL,
+  price decimal(9,2) NOT NULL
 );
 
-create table zip (
-    zip integer primary key,
-    city varchar(35),
-    state_two_letter_code char(2)
+CREATE TABLE Inventory (
+  machine_id integer,
+  product_id integer,
+  items_left integer,
+  FOREIGN KEY (machine_id) REFERENCES Machines (id),
+  FOREIGN KEY (product_id) REFERENCES Products (id)
 );
 
-create table dog (
-    dog_id integer primary key,
-    name varchar(35),
-    breed varchar(35),
-    birth_date date
-);
-
-create table award (
-    award_id integer primary key,
-    dog_id integer not null,
-    event_date date,
-    award_name varchar(25) not null,
-    foreign key (dog_id) references dog (dog_id)
-);
-
-create table person_dog (
-    dog_id integer,
-    person_id integer,
-    foreign key (dog_id) references dog (dog_id),
-    foreign key (person_id) references person (person_id)
+CREATE TABLE Transactions (
+  id integer PRIMARY KEY AUTO_INCREMENT,
+  location_id integer NOT NULL,
+  machine_id integer NOT NULL,
+  product_id integer NOT NULL,
+  transaction_time datetime NOT NULL,
+  FOREIGN KEY (location_id) REFERENCES Locations (id),
+  FOREIGN KEY (machine_id) REFERENCES Machines (id),
+  FOREIGN KEY (product_id) REFERENCES Products (id)
 );
